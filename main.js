ad0059ecf3c2e2370d32a144e04aef7c30d9651e
V-Goo <v-go@ya.ru>
2020-09-11 14:42:40 +0300

    three day commit

--- main.js
+++ main.js
@@ -28,6 +28,8 @@ const  score = document.querySelector('.score'),
 
   function startGame() {
     start.classList.add('hide');
+    gameArea.innerHTML = '';
+    
     for (let i = 0; i < getQuantityElements(100); i++) {
       const line = document.createElement('div');
       line.classList.add('line');
@@ -46,8 +48,12 @@ const  score = document.querySelector('.score'),
       gameArea.appendChild(enemy)
     }
 
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -55,6 +61,8 @@ const  score = document.querySelector('.score'),
 
   function playGame(){
     if (setting.start) {
+      setting.score += setting.speed;
+      score.innerHTML = 'SCORE<br>' + setting.score;
       moveRoad();
       moveEnemy();
       if (keys.ArrowLeft && setting.x > 0) {
@@ -105,7 +113,21 @@ const  score = document.querySelector('.score'),
 
   function moveEnemy(){
     let enemy = document.querySelectorAll('.enemy');
+
     enemy.forEach(function(item) {
+      let carRect = car.getBoundingClientRect();
+      let enemyRect = item.getBoundingClientRect();
+
+      if(carRect.top <= enemyRect.bottom &&
+        carRect.right >= enemyRect.left &&
+        carRect.left <= enemyRect.right &&
+        carRect.bottom >= enemyRect.top) {
+          setting.start = false;
+          console.warn('FFFFF');
+          start.classList.remove('hide');
+          start.style.top = score.offsetHeight;
+
+      }
       item.y += setting.speed / 2;
       item.style.top = item.y + 'px';
       
